01c9cd64bb99866e8004f2b1055a1dea1a8bb7d0
tech-kelly <78743080+duck-tech@users.noreply.github.com>
2022-05-15 19:36:36 +0800

    first commit

---
+++ apps/potter/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'w12-potter-kata-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'potter';
+}
