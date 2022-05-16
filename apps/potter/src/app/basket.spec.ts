19e4e1df95a1ab32ea23be3217b0e57eb247afee
tech-kelly <78743080+duck-tech@users.noreply.github.com>
2022-05-16 18:18:38 +0800

    第四個紅燈

--- apps/potter/src/app/basket.spec.ts
+++ apps/potter/src/app/basket.spec.ts
@@ -34,4 +34,10 @@ describe("Basket", () => {
     expect(basket.buy([0, 0, 1, 2, 2, 3])).toBe((8 * 4 * 0.8) + (8 * 2 * 0.95))
     expect(basket.buy([0, 1, 1, 2, 3, 4])).toBe(8 + (8 * 5 * 0.75))
   })
+
+  test('testEdgeCases', () => {
+    expect(basket.buy([0, 0, 1, 1, 2, 2, 3, 4])).toBe(2 * (8 * 4 * 0.8))
+    expect(basket.buy([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4,])
+    ).toBe(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8));
+  })
 })
