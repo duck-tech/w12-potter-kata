db6df929b63a2c23bf8e45495c377bb5a391cf3f
tech-kelly <78743080+duck-tech@users.noreply.github.com>
2022-05-16 17:34:34 +0800

    第二個紅燈

--- apps/potter/src/app/basket.spec.ts
+++ apps/potter/src/app/basket.spec.ts
@@ -15,4 +15,12 @@ describe('Basket', () => {
     expect(basket.buy([4])).toBe(8);
     expect(basket.buy([1, 1, 1])).toBe(24);
   });
+
+  test('testSimpleDiscounts', () => {
+    const basket = new Basket()
+    expect(basket.buy([0, 1])).toBe(8 * 2 * 0.95);
+    expect(basket.buy([0, 2, 4])).toBe(8 * 3 * 0.9);
+    expect(basket.buy([0, 1, 2, 4])).toBe(8 * 4 * 0.8);
+    expect(basket.buy([0, 1, 2, 3, 4])).toBe(8 * 5 * 0.75);
+  });
 });
