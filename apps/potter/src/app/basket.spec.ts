8d36d7c819aa7661282c9162db0c7ede4c401ab7
tech-kelly <78743080+duck-tech@users.noreply.github.com>
2022-05-16 18:09:30 +0800

    第二次重構

--- apps/potter/src/app/basket.spec.ts
+++ apps/potter/src/app/basket.spec.ts
@@ -1,37 +1,37 @@
-import { TestBed } from '@angular/core/testing';
-import { Basket } from './basket';
+import { TestBed } from '@angular/core/testing'
+import { Basket } from './basket'
 
 describe("Basket", () => {
-  let basket: Basket;
+  let basket: Basket
 
   beforeEach(() => {
     basket = new Basket();
-  });
+  })
 
   it("should create an instance", () => {
     expect(basket).toBeTruthy();
-  });
+  })
 
   test("testBasics", () => {
-    expect(basket.buy([])).toBe(0);
-    expect(basket.buy([1])).toBe(8);
-    expect(basket.buy([2])).toBe(8);
-    expect(basket.buy([3])).toBe(8);
-    expect(basket.buy([4])).toBe(8);
-    expect(basket.buy([1, 1, 1])).toBe(24);
-  });
+    expect(basket.buy([])).toBe(0)
+    expect(basket.buy([1])).toBe(8)
+    expect(basket.buy([2])).toBe(8)
+    expect(basket.buy([3])).toBe(8)
+    expect(basket.buy([4])).toBe(8)
+    expect(basket.buy([1, 1, 1])).toBe(24)
+  })
 
   test("testSimpleDiscounts", () => {
-    expect(basket.buy([0, 1])).toBe(8 * 2 * 0.95);
-    expect(basket.buy([0, 2, 4])).toBe(8 * 3 * 0.9);
-    expect(basket.buy([0, 1, 2, 4])).toBe(8 * 4 * 0.8);
-    expect(basket.buy([0, 1, 2, 3, 4])).toBe(8 * 5 * 0.75);
-  });
+    expect(basket.buy([0, 1])).toBe(8 * 2 * 0.95)
+    expect(basket.buy([0, 2, 4])).toBe(8 * 3 * 0.9)
+    expect(basket.buy([0, 1, 2, 4])).toBe(8 * 4 * 0.8)
+    expect(basket.buy([0, 1, 2, 3, 4])).toBe(8 * 5 * 0.75)
+  })
 
   test('testSeveralDiscounts', () => {
-    expect(basket.buy([0, 0, 1])).toBe(8 + (8 * 2 * 0.95));
-    expect(basket.buy([0, 0, 1, 1])).toBe(2 * (8 * 2 * 0.95));
-    expect(basket.buy([0, 0, 1, 2, 2, 3])).toBe((8 * 4 * 0.8) + (8 * 2 * 0.95));
-    expect(basket.buy([0, 1, 1, 2, 3, 4])).toBe(8 + (8 * 5 * 0.75));
-  });
-});
+    expect(basket.buy([0, 0, 1])).toBe(8 + (8 * 2 * 0.95))
+    expect(basket.buy([0, 0, 1, 1])).toBe(2 * (8 * 2 * 0.95))
+    expect(basket.buy([0, 0, 1, 2, 2, 3])).toBe((8 * 4 * 0.8) + (8 * 2 * 0.95))
+    expect(basket.buy([0, 1, 1, 2, 3, 4])).toBe(8 + (8 * 5 * 0.75))
+  })
+})
--- apps/potter/src/app/basket.ts
+++ apps/potter/src/app/basket.ts
@@ -1,45 +1,52 @@
 export class Basket {
-  bookMap!: number[];
-  perBookPrice = 8;
-  discount = [1, 1, 0.95, 0.9, 0.8, 0.75];
+  bookMap!: number[]
+  perBookPrice = 8
+  discount = [1, 1, 0.95, 0.9, 0.8, 0.75]
 
   buy(orders: Array<number>) {
-    let price = 0;
-    this.bookMap = [0, 0, 0, 0, 0];
-    this.addOrdersToBookMap(orders);
+    let price = 0
+    this.bookMap = [0, 0, 0, 0, 0]
+    this.addOrdersToBookMap(orders)
 
     if (this.getBookCount() < 2) {
       price = orders.length * this.perBookPrice;
     } else {
-      for (let i = 0; i < this.bookMap.length; i++ ){
-        if (this.bookMap[i] > 0) {
-          price += this.perBookPrice * this.getBookCount() *
+      while (this.checkBookMapLeft()) {
+        price += this.perBookPrice * this.getBookCount() *
           this.discount[this.getBookCount()];
           this.updateBookMap()
-          i -= 1
-        }
-      } 
+      }
     }
-    return price;
+    return price
   }
 
   private addOrdersToBookMap(orders: number[]) {
     orders.forEach((bookNumber) => {
-      this.bookMap[bookNumber] += 1;
-    });
+      this.bookMap[bookNumber] += 1
+    })
   }
 
   private getBookCount() {
-    let count = 0;
+    let count = 0
     this.bookMap.forEach((bookNumber) => {
-      if (bookNumber > 0) count++;
-    });
+      if (bookNumber > 0) count++
+    })
     return count;
   }
 
+  private checkBookMapLeft() {
+    for (let i = 0; i < this.bookMap.length; i++){
+      if (this.bookMap[i] > 0) {
+        return true
+      }
+    }
+    return false
+  }
   private updateBookMap() {
     this.bookMap = this.bookMap.map((bookNumber) => {
       return bookNumber - 1
     }) 
   }
+
+  
 }
