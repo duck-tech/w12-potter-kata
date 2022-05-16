78d7ed47dd53a0044d82bacb2d7b63169dbcdf9d
tech-kelly <78743080+duck-tech@users.noreply.github.com>
2022-05-16 17:37:08 +0800

    第二個綠燈

--- apps/potter/src/app/basket.ts
+++ apps/potter/src/app/basket.ts
@@ -1,11 +1,28 @@
 export class Basket {
-  buy(orders:Array<number>){
-    const perBookPrice = 8
+  buy(orders: Array<number>) {
+    const perBookPrice = 8;
+    const discount = [1, 1, 0.95, 0.9, 0.8, 0.75];
+    const bookMap = [0, 0, 0, 0, 0];
     let price = 0;
-    
-    for(let i = 0; i < orders.length; i++){
-      price += perBookPrice
+    let count = 0;
+
+    for (let i = 0; i < orders.length; i++) {
+      bookMap[orders[i]] += 1;
     }
-    return price
+
+    for (let i = 0; i < bookMap.length; i++) {
+      if (bookMap[i] > 0) {
+        count++;
+      }
+    }
+
+    if (count === 1 || 0) {
+      for (let i = 0; i < orders.length; i++) {
+        price += perBookPrice;
+      }
+    } else {
+        price = perBookPrice * count * discount[count];
+      }
+      return price;
+    }
   }
-}
