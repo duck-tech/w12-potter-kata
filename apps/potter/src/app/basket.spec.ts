98de97f173cb8863d5b8b71f9150668907ecac3c
tech-kelly <78743080+duck-tech@users.noreply.github.com>
2022-05-16 17:47:39 +0800

    第一次重構

--- apps/potter/src/app/basket.spec.ts
+++ apps/potter/src/app/basket.spec.ts
@@ -1,13 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { Basket } from './basket';
 
-describe('Basket', () => {
-  it('should create an instance', () => {
-    expect(new Basket()).toBeTruthy();
+describe("Basket", () => {
+  let basket: Basket;
+
+  beforeEach(() => {
+    basket = new Basket();
   });
 
-  test('testBasics', () => {
-    const basket = new Basket()
+  it("should create an instance", () => {
+    expect(basket).toBeTruthy();
+  });
+
+  test("testBasics", () => {
     expect(basket.buy([])).toBe(0);
     expect(basket.buy([1])).toBe(8);
     expect(basket.buy([2])).toBe(8);
@@ -16,8 +21,7 @@ describe('Basket', () => {
     expect(basket.buy([1, 1, 1])).toBe(24);
   });
 
-  test('testSimpleDiscounts', () => {
-    const basket = new Basket()
+  test("testSimpleDiscounts", () => {
     expect(basket.buy([0, 1])).toBe(8 * 2 * 0.95);
     expect(basket.buy([0, 2, 4])).toBe(8 * 3 * 0.9);
     expect(basket.buy([0, 1, 2, 4])).toBe(8 * 4 * 0.8);
--- apps/potter/src/app/basket.ts
+++ apps/potter/src/app/basket.ts
@@ -1,28 +1,35 @@
 export class Basket {
+  bookMap!: number[];
+  perBookPrice = 8;
+  discount = [1, 1, 0.95, 0.9, 0.8, 0.75];
+
   buy(orders: Array<number>) {
-    const perBookPrice = 8;
-    const discount = [1, 1, 0.95, 0.9, 0.8, 0.75];
-    const bookMap = [0, 0, 0, 0, 0];
     let price = 0;
-    let count = 0;
+    this.bookMap = [0, 0, 0, 0, 0];
+    this.addOrdersToBookMap(orders);
 
-    for (let i = 0; i < orders.length; i++) {
-      bookMap[orders[i]] += 1;
-    }
-
-    for (let i = 0; i < bookMap.length; i++) {
-      if (bookMap[i] > 0) {
-        count++;
-      }
-    }
-
-    if (count === 1 || 0) {
-      for (let i = 0; i < orders.length; i++) {
-        price += perBookPrice;
-      }
+    if (this.getBookCount() < 2) {
+      price = orders.length * this.perBookPrice;
     } else {
-        price = perBookPrice * count * discount[count];
-      }
-      return price;
+      price =
+        this.perBookPrice *
+        this.getBookCount() *
+        this.discount[this.getBookCount()];
     }
+    return price;
   }
+
+  private addOrdersToBookMap(orders: number[]) {
+    orders.forEach((bookNumber) => {
+      this.bookMap[bookNumber] += 1;
+    });
+  }
+
+  private getBookCount() {
+    let count = 0;
+    this.bookMap.forEach((bookNumber) => {
+      if (bookNumber > 0) count++;
+    });
+    return count;
+  }
+}
